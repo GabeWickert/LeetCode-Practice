44f6d3defe00eb750b6eb7dca6dd8e97e742e7b9
GabeWickert <150979856+GabeWickert@users.noreply.github.com>
2024-06-25 20:29:55 -0700

    Time: 59 ms (7.85%), Space: 11.5 MB (89.82%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -4,7 +4,7 @@ class ListNode:
         self.next = next
 
 class Solution:
-    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+    def addTwoNumbers(self, l1, l2):
         dummy_head = ListNode()
         current = dummy_head
         carry = 0
